e2c3f2cf9d6fd04d49f97115928239528ec668df
= <=>
2021-08-26 16:16:30 +0545

    The changes has been made,

---
+++ app/Models/Petition.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Petition extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title','description','category','author','signees',
+    ];
+
+
+
+}
---
+++ database/seeders/PetitionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Petition;
+use Database\Factories\PetitionFactory;
+use Illuminate\Database\Seeder;
+
+class PetitionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Petition::factory()->times(50)->create();
+    }
+}
